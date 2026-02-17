432911cd1ebedb982c651e1001b16e70c86aec0f
re2osushi8888 <re.2osushi8888@gmail.com>
2026-02-17 19:27:02 +0900

    日本語のコピペがバグるのでclipboardをwin32yankに変更

--- config/.config/nvim/lua/config/clipboard.lua
+++ config/.config/nvim/lua/config/clipboard.lua
@@ -1,15 +1,25 @@
 -- クリップボード連携
 if vim.fn.has("wsl") == 1 then
   vim.g.clipboard = {
-    name = "WslClipboard",
+    -- name = "WslClipboard",
+    -- copy = {
+    --   ["+"] = { "bash", "-lc", "iconv -f utf-8 -t utf-16le | clip.exe" },
+    --   ["*"] = { "bash", "-lc", "iconv -f utf-8 -t utf-16le | clip.exe" },
+    -- },
+    -- paste = {
+    --   ["+"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    --   ["*"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    -- },
+    -- cache_enabled = 0,
+    name = "win32yank-wsl",
     copy = {
-      ["+"] = { "bash", "-lc", "iconv -f utf-8 -t utf-16le | clip.exe" },
-      ["*"] = { "bash", "-lc", "iconv -f utf-8 -t utf-16le | clip.exe" },
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
     },
     paste = {
-      ["+"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-      ["*"] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
     },
-    cache_enabled = 0,
+    cache_enabled = 1,
   }
 end
