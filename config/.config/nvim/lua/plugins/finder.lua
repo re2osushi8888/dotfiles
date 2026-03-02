0990d8e624d671bbf1444a5d04efe0326225e1ef
re2osushi8888 <re.2osushi8888@gmail.com>
2026-03-02 19:18:35 +0900

    telescopeの設定を調整

--- config/.config/nvim/lua/plugins/finder.lua
+++ config/.config/nvim/lua/plugins/finder.lua
@@ -2,5 +2,28 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          -- プレビューの表示設定
+          layout_strategy = "horizontal",
+          layout_config = {
+            horizontal = {
+              preview_width = 0.55,
+            },
+          },
+        },
+        pickers = {
+          find_files = {
+            -- 隠しファイルも表示
+            hidden = true,
+            -- .gitignoreを無視してすべてのファイルを表示する場合は以下をコメント解除
+            -- no_ignore = true,
+            -- より高速な検索のためにfdを使用（インストールされている場合）
+            find_command = { "rg", "--files", "--hidden", "--glob", "!.git/*" },
+          },
+        },
+      })
+    end,
   },
 }
